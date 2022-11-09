a14b7159d8f0780809220a56b91ff04d152df146
aurrelkondi <aurrelkondi@hotmail.com>
2022-11-09 15:43:11 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${appId}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${appId}`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -36,7 +36,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F </h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C </h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
